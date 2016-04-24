6725cdab2cc25f5680e9f80cb4188d9cf984401a
Terry Hayes <terrychayes@gmail.com>
2016-04-24 15:56:59 -0400

    Finished the Udactity instructional walkthrough. Added Gemfile, errors, added function to name untitled list if no name is given.

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,14 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+
+  def format_date(dates={})
+  	#This method should take in either 1 or 2 dates since TodoItem requires formatting 1 date, while EventItem could have up to 2 dates
+
+  end
+
+  def format_priority
+  end
+
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -3,15 +3,25 @@ class UdaciList
 
   def initialize(options={})
     @title = options[:title]
+    if !options[:title]
+      @title = "Untitled List"
+    end
     @items = []
   end
   def add(type, description, options={})
     type = type.downcase
+    raise UdaciListErrors::InvalidItemType unless ["todo", "event", "link"].include?(type)
+    if options[:priority]
+      raise UdaciListErrors::InvalidPriorityValue if !["high", "medium", "low"].include?(options[:priority]) #Messy code; trying to say!include?
+    end
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
   end
   def delete(index)
+    if index > @items.length
+      raise UdaciListErrors::IndexExceedsListSize
+    end
     @items.delete_at(index - 1)
   end
   def all
